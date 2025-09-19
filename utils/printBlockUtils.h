5d79aafff99c4dc78f9536dee92ed6260f39f698
rodut11 <rodut11@protonmail.com>
2025-09-19 22:08:02 +0300

    Update printBlockUtils.h

--- utils/printBlockUtils.h
+++ utils/printBlockUtils.h
@@ -1,7 +1,3 @@
-//
-// Created by rodut11 on 9/19/25.
-//
-
 #ifndef PRINTBLOCKUTILS_H
 #define PRINTBLOCKUTILS_H
 #include "../blocks.h"
@@ -9,4 +5,4 @@
 void PrintGrid(int grid[MAX_GRID_HEIGHT][MAX_GRID_WIDTH]);
 void PrintBlock(Block block);
 
-#endif //C_PRINTBLOCKUTILS_H
+#endif //PRINTBLOCKUTILS_H
