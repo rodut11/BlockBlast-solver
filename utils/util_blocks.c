15dc4400f2606bda0afd995eb71eb1022c640f68
Tataru Tudor Sorin <rodut11@protonmail.com>
2025-09-21 16:29:07 +0300

    added color codes header file

--- utils/util_blocks.c
+++ utils/util_blocks.c
@@ -6,7 +6,7 @@ void grid_print(int grid[MAX_GRID_HEIGHT][MAX_GRID_WIDTH]) {
 
     for(int i = 0; i < MAX_GRID_HEIGHT; i++) {
         for(int j = 0; j < MAX_GRID_WIDTH; j++) {
-            printf("%d ", grid[i][j]); // print the value
+            printf("%d |", grid[i][j]); // print the value
         }
         printf("\n"); // new line after each row
     }
